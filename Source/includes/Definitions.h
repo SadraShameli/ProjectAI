2d587c6c8942d22d7026e8c13b722b385199c61a
Sadra Shameli <sadra.shameli1@gmail.com>
2022-09-11 22:11:27 +0200

    Added CMake

---
+++ Source/includes/Definitions.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/*
+#include <iostream>
+
+#include <vector>
+#include <array>
+
+#include <string>
+#include <string_view>
+*/
---
+++ Source/src/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hi\n";
+
+    return 0;
+}
